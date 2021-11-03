a5221e83c6d26e78e6316c426333293385346023
Anna Fabrichnaya <fabrichnaya.anna@bk.ru>
2021-11-03 21:28:19 +0300

    Initial commit

---
+++ src/com/company/Netology/Main.java
@@ -0,0 +1,11 @@
+package com.company.Netology;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BinOps bin = new BinOps();
+        System.out.println(bin.mult("111", "11"));
+        System.out.println(bin.sum("001", "101"));
+
+    }
+}
